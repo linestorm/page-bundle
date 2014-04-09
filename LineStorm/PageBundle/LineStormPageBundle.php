864bfe4600972645807d17cedaf5cab69c2cf97a
Andy Thorne <contrabandvr@gmail.com>
2014-04-09 21:52:17 +0000

    Initial commit

---
+++ LineStorm/PageBundle/LineStormPageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LineStorm\PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LineStormPageBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Page Module for LineStorm Post
+=================================
+
+Page Module for the LineStorm Post for symfony >=2.3
